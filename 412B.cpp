5a883c17177db3ba459e3a8ec02c1d9613986b0f
Rushil Khullar <105356056+sayRushil@users.noreply.github.com>
2022-11-21 09:26:15 +0530

    Network Configuration-412B
    Accepted. Rating-900

---
+++ 412B.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k; cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    sort(v.rbegin(),v.rend());
+    cout<<v[--k];
+}
